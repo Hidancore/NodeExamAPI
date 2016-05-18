67dda8d259cd7b9167c97a3834797502b2364eca
Hidancore <nicklasandersen1@live.dk>
2016-05-18 15:33:43 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,149 @@
 /**
  * Created by Alex on 18-05-2016.
  */
+
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var BodyParser = require('body-parser'); // middle
 
-var url = 'mongodb://Alex:2108@ds025232.mlab.com:25232/nodeandroid';
+var url = 'mongodb://nick2463:kea240993#@ds025232.mlab.com:25232/nodeandroid';
 
 app.use(BodyParser.urlencoded({
     extended: true
 }));
 app.use(BodyParser.json());
 
+app.get('/movies', function(req, res) {
 
+    MongoClient.connect(url, function(err, db){
+        //Error in DB; return 500
+        if(err){
+            res.status(500);
+            res.json({
+                'error':'Internal database error'
+            });
+
+            var collection = db.collection('movies');
+        } try{
+            var col = db.collection('movies');
+            col.find().toArray(function(err, result){
+
+                if(err){
+                    res.status(500);
+                    res.json({
+                        'error': 'Internal database error'
+                    })
+                }else if (result !== null){
+                    res.status(200);
+                    res.json(result)
+                } else {
+                    res.status(404)
+                    res.json({
+                        "msg" : "empty"
+                    })
+                }
+
+                db.close();
+            });
+        }
+    });
+
+});
+
+
+
+// Route that handles creation of new user
+
+app.delete('/movies/:id', function(req, res) {
+
+    MongoClient.connect(url, function(err, db){
+        var col = db.collection('movies');
+        col.remove({
+            '_id' : ObjectID(req.params.id)
+        }, function(err, result){
+            if(err){
+                res.status(500);
+                res.json({
+                    "error" : "Internal Server Error"
+                })
+            } else{
+                res.status(202);
+                res.json({
+                    "msg" : "movie deleted"
+                });
+            }
+        });
+        db.close();
+    });
+
+});
+
+// Route that handles creation of new user
+
+app.post('/movies', function(req, res) {
+
+    MongoClient.connect(url, function(err,db){
+        if(err){
+            res.status(500);
+            res.json({
+                "error": "Internal Server Error"
+            });
+        }else {
+            var col = db.collection('movies');
+
+            col.insert(req.body, function(err, result){
+                if(err){
+                    res.status(500);
+                    res.json({
+                        "error": "Internal Server Error"
+                    })
+                } else if(result === null){
+                    res.status(404).send({
+                        "msg" : "404"
+                    })
+
+                } else {
+                    res.status(201);
+                    res.json({
+                        "msg" : "movies created"
+                    })
+                }
+                db.close();
+            });
+
+        }
+
+
+    })
+
+});
+
+// Route that handles updates of a user
+
+app.put('/movies/:id', function(req, res) {
+    MongoClient.connect(url, function(err, db){
+        if(err){
+            res.status(500);
+            res.json({
+                "error" : "Internal Server Error"
+            })
+        } else{
+            var col = collection.update({
+                '_id' : ObjectID(req.params.id)
+            }, {
+                $set: req.body
+            }, function(err, result){
+                res.status(201);
+                res.json({
+                    "msg" : "movie updated"
+                });
+                db.close();
+            });
+        }
+    })
+});
 
 app.use('/',function(req, res) {
     res.status(404);
